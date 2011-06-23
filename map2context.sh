04e625a523e93efef1c3fec599d04fd5577d20ad
Glynn Naughton <glynn@wordprovider.co.uk>
2011-06-23 10:07:31 +0100

    Add map2context.sh

---
+++ map2context.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DITAMAP="orm/en/ope_map_root.ditamap"
+OUTPUT="output/out.tex"
+XSLT="xslt/map2context.xsl"
+
+rm output/*.*
+
+echo Transforming ditamap...
+xsltproc --novalid -o $OUTPUT $XSLT $DITAMAP 
+echo Transformation finished.
+
+echo Creating PDF...
+cd output
+texexec out.tex
+
+evince out.pdf
